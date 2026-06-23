5deb06b3ec876caa950c61ad273cc2788abfc3ca
dlguszoo <lhjivy1024@gmail.com>
2026-06-23 21:48:50 +0900

    [#1] chore: 기본 App 모듈 세팅

---
+++ Projects/ChungBazi/Project.swift
@@ -0,0 +1,46 @@
+import ProjectDescription
+import ProjectDescriptionHelpers
+
+let project = Project.project(
+    name: BaziModule.ChungBazi.name,
+    targets: [
+        .target(
+            name: BaziModule.ChungBazi.name,
+            product: .app,
+            bundleId: Project.bundleID,
+            infoPlist: .extendingDefault(with: [
+                "CFBundleDisplayName": .string("청바지"),
+                "CFBundleShortVersionString": .string("1.0.0"),
+                "CFBundleVersion": .string("1"),
+                "UILaunchStoryboardName": .string("LaunchScreen"),
+                "UISupportedInterfaceOrientations": .array([.string("UIInterfaceOrientationPortrait")]),
+                "UIApplicationSceneManifest": .dictionary([
+                    "UIApplicationSupportsMultipleScenes": .boolean(false),
+                ]),
+            ]),
+            sources: .sources,
+            resources: .default,
+            entitlements: .file(path: "Resources/ChungBazi.entitlements"),
+            dependencies: [
+                .presentation(),
+                .data(),
+                .domain(),
+                .design(),
+                .core(),
+            ],
+            settings: .settings(
+                configurations: [
+                    .debug(name: "Debug", xcconfig: .relativeToRoot("Projects/ChungBazi/Configurations/Debug.xcconfig")),
+                    .release(name: "Release", xcconfig: .relativeToRoot("Projects/ChungBazi/Configurations/Release.xcconfig")),
+                ]
+            )
+        ),
+    ],
+    schemes: [
+        .scheme(
+            name: BaziModule.ChungBazi.name,
+            buildAction: .buildAction(targets: [.target(BaziModule.ChungBazi.name)]),
+            runAction: .runAction(executable: .target(BaziModule.ChungBazi.name))
+        ),
+    ]
+)
