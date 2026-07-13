c0b16e0d83d65f6b85b3e6b909e787da77f257ee
dlguszoo <lhjivy1024@gmail.com>
2026-07-14 02:40:14 +0900

    [#9] chore: 최소 지원 버전 17.0으로 수정 및 copyright 입력

--- Projects/BaziDesign/Sources/Extensions/Radius/BaziRadius.swift
+++ Projects/BaziDesign/Sources/Extensions/Radius/BaziRadius.swift
@@ -1,3 +1,5 @@
+// Copyright © 2026 ChungBazi. All rights reserved.
+
 import CoreGraphics
 
 public enum BaziRadius: CGFloat, CaseIterable {
--- Projects/BaziDesign/Sources/Foundation/Fonts/FontRegistrator.swift
+++ Projects/BaziDesign/Sources/Foundation/Fonts/FontRegistrator.swift
@@ -1,3 +1,5 @@
+// Copyright © 2026 ChungBazi. All rights reserved.
+
 import CoreText
 import Foundation
 
