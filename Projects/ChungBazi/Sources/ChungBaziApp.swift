f90bbcd3a735d0ebe5c47829ec139c9a6518f156
dlguszoo <lhjivy1024@gmail.com>
2026-07-13 19:59:12 +0900

    [#5] feat: sample app, default app에서 register fonts 메서드 호출

--- Projects/ChungBazi/Sources/ChungBaziApp.swift
+++ Projects/ChungBazi/Sources/ChungBaziApp.swift
@@ -1,7 +1,12 @@
+import BaziDesign
 import SwiftUI
 
 @main
 struct ChungBaziApp: App {
+    init() {
+        FontRegistrator.registerFonts()
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
