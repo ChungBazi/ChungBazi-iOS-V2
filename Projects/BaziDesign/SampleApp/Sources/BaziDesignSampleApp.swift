99fbdfe011b2070bb55e5507e4ecbabe02f7ed07
dlguszoo <lhjivy1024@gmail.com>
2026-07-13 19:59:12 +0900

    [#5] feat: sample app, default app에서 register fonts 메서드 호출

--- Projects/BaziDesign/SampleApp/Sources/BaziDesignSampleApp.swift
+++ Projects/BaziDesign/SampleApp/Sources/BaziDesignSampleApp.swift
@@ -1,7 +1,12 @@
+import BaziDesign
 import SwiftUI
 
 @main
 struct BaziDesignSampleApp: App {
+    init() {
+        FontRegistrator.registerFonts()
+    }
+
     var body: some Scene {
         WindowGroup {
             Text("BaziDesign Sample")
