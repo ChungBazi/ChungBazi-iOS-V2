a80e1c1bb43f97b388728dbb57bb706f25cc6d46
dlguszoo <lhjivy1024@gmail.com>
2026-07-14 18:08:11 +0900

    [#8] fix: response DTO 수동 이니셜라이저 제거

--- Projects/BaziNetwork/Sources/Home/PolicyListResponseDTO.swift
+++ Projects/BaziNetwork/Sources/Home/PolicyListResponseDTO.swift
@@ -8,13 +8,6 @@ public struct PolicyListResponseDTO: Decodable {
     public let policies: [PolicyItemDTO]
     public let nextCursor: String
     public let hasNext: Bool
-    
-    init(totalCount: Int, policies: [PolicyItemDTO], nextCursor: String, hasNext: Bool) {
-        self.totalCount = totalCount
-        self.policies = policies
-        self.nextCursor = nextCursor
-        self.hasNext = hasNext
-    }
 }
 
 public struct PolicyItemDTO: Decodable {
@@ -25,14 +18,4 @@ public struct PolicyItemDTO: Decodable {
     public let title: String
     public let viewCount: Int
     public let liked: Bool
-    
-    init(policyId: Int, category: String, categoryName: String, dDay: String, title: String, viewCount: Int, liked: Bool) {
-        self.policyId = policyId
-        self.category = category
-        self.categoryName = categoryName
-        self.dDay = dDay
-        self.title = title
-        self.viewCount = viewCount
-        self.liked = liked
-    }
 }
--- Projects/BaziNetwork/Sources/User/UserResponseDTO.swift
+++ Projects/BaziNetwork/Sources/User/UserResponseDTO.swift
@@ -10,26 +10,10 @@ public struct PolicyProfileResponseDTO: Decodable {
     public let employmentCode: String
     public let incomeLevel: String
     public let interestCategories: [String]
-    
-    init(birth: String, sidoCode: String, sigunguCode: String, educationCode: String, employmentCode: String, incomeLevel: String, interestCategories: [String]) {
-        self.birth = birth
-        self.sidoCode = sidoCode
-        self.sigunguCode = sigunguCode
-        self.educationCode = educationCode
-        self.employmentCode = employmentCode
-        self.incomeLevel = incomeLevel
-        self.interestCategories = interestCategories
-    }
 }
 
 public struct UserInfoResponseDTO: Decodable {
     public let name: String
     public let email: String
     public let socialType: String
-    
-    init(name: String, email: String, socialType: String) {
-        self.name = name
-        self.email = email
-        self.socialType = socialType
-    }
 }
