c22bc44d18858e36d4c626160e0dcb3dd08fc821
dlguszoo <lhjivy1024@gmail.com>
2026-07-14 16:15:02 +0900

    [#8] feat: 네트워크 통신 BaseURL 설정 (xcconfig 파일로 관리)

---
+++ Projects/BaziData/Sources/DataConfiguration.swift
@@ -0,0 +1,9 @@
+// Copyright © 2026 ChungBazi. All rights reserved.
+
+import BaziNetwork
+
+public struct DataConfiguration {
+    public static func configure(baseURL: String) {
+        APIDomain.configure(baseURL: baseURL)
+    }
+}
