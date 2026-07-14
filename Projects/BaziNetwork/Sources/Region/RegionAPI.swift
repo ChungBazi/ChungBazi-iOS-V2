0e40fd0b4332d54431cee509868dd3173260b425
dlguszoo <lhjivy1024@gmail.com>
2026-07-14 16:16:52 +0900

    [#8] feat: Region API/DTO 구현

---
+++ Projects/BaziNetwork/Sources/Region/RegionAPI.swift
@@ -0,0 +1,34 @@
+// Copyright © 2026 ChungBazi. All rights reserved.
+
+import Foundation
+import Moya
+
+public enum RegionAPI {
+    case getSido
+    case getSigungu(sido: String)
+}
+
+extension RegionAPI: APITargetType {
+    public var baseURL: URL { URL(string: APIDomain.regionURL)! }
+    
+    public var path: String {
+        switch self {
+        case .getSido:    return "/sido"
+        case .getSigungu: return "/sigungu"
+        }
+    }
+    
+    public var method: Moya.Method { .get }
+    
+    public var task: Task {
+        switch self {
+        case .getSido:
+            return .requestPlain
+        case .getSigungu(let sido):
+            return .requestParameters(
+                parameters: ["sido": sido],
+                encoding: URLEncoding.queryString
+            )
+        }
+    }
+}
---
+++ Projects/BaziNetwork/Sources/Region/RegionResponseDTO.swift
@@ -0,0 +1,13 @@
+// Copyright © 2026 ChungBazi. All rights reserved.
+
+import Foundation
+
+public struct SidoResponseDTO: Decodable {
+    public let sidoCode: String
+    public let sidoName: String
+}
+
+public struct SigunguResponseDTO: Decodable {
+    public let sigunguCode: String
+    public let sigunguName: String
+}
