a51ccfe4cf55bf07f81fdf982ca4c2fb014aad85
dlguszoo <lhjivy1024@gmail.com>
2026-07-14 16:15:44 +0900

    [#8] feat: Auth API/DTO 구현

---
+++ Projects/BaziNetwork/Sources/Auth/AuthRequestDTO.swift
@@ -0,0 +1,25 @@
+// Copyright © 2026 ChungBazi. All rights reserved.
+
+import Foundation
+
+public struct KakaoLoginRequestDTO: Encodable {
+    public let accessToken: String
+    public let fcmToken: String
+
+    public init(accessToken: String, fcmToken: String) {
+        self.accessToken = accessToken
+        self.fcmToken = fcmToken
+    }
+}
+
+public struct AppleLoginRequestDTO: Encodable {
+    public let idToken: String
+    public let name: String
+    public let fcmToken: String
+    
+    public init(idToken: String, name: String, fcmToken: String) {
+        self.idToken = idToken
+        self.name = name
+        self.fcmToken = fcmToken
+    }
+}
