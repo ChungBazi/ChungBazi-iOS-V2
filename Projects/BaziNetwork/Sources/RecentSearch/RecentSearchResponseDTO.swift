52a799b204d4aec829b3dc61d34ab1fc308818ac
dlguszoo <lhjivy1024@gmail.com>
2026-07-14 16:20:14 +0900

    [#8] fix: DTO init함수 추가

--- Projects/BaziNetwork/Sources/RecentSearch/RecentSearchResponseDTO.swift
+++ Projects/BaziNetwork/Sources/RecentSearch/RecentSearchResponseDTO.swift
@@ -7,9 +7,21 @@ public struct RecentSearchResponseDTO: Decodable {
     public let keyword: [KeywordDTO]
     public let nextCursor: String
     public let hasNext: Bool
+    
+    init(autoSaveEnabled: Bool, keyword: [KeywordDTO], nextCursor: String, hasNext: Bool) {
+        self.autoSaveEnabled = autoSaveEnabled
+        self.keyword = keyword
+        self.nextCursor = nextCursor
+        self.hasNext = hasNext
+    }
 }
 
 public struct KeywordDTO: Decodable {
     public let keywordId: Int
     public let keyword: String
+    
+    init(keywordId: Int, keyword: String) {
+        self.keywordId = keywordId
+        self.keyword = keyword
+    }
 }
--- Projects/BaziNetwork/Sources/Region/RegionResponseDTO.swift
+++ Projects/BaziNetwork/Sources/Region/RegionResponseDTO.swift
@@ -5,9 +5,19 @@ import Foundation
 public struct SidoResponseDTO: Decodable {
     public let sidoCode: String
     public let sidoName: String
+    
+    init(sidoCode: String, sidoName: String) {
+        self.sidoCode = sidoCode
+        self.sidoName = sidoName
+    }
 }
 
 public struct SigunguResponseDTO: Decodable {
     public let sigunguCode: String
     public let sigunguName: String
+    
+    init(sigunguCode: String, sigunguName: String) {
+        self.sigunguCode = sigunguCode
+        self.sigunguName = sigunguName
+    }
 }
