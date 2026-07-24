c97e858013bd4f67cc3861e3509254db01dc1065
dlguszoo <lhjivy1024@gmail.com>
2026-07-25 00:58:09 +0900

    [#18] fix: 수정된 와이어프레임 반영한 Route 구현

--- Projects/BaziPresentation/Sources/Coordinator/Routes/ProfileRoute.swift
+++ Projects/BaziPresentation/Sources/Coordinator/Routes/ProfileRoute.swift
@@ -1,12 +1,14 @@
 // Copyright © 2026 ChungBazi. All rights reserved.
 
 public enum ProfileRoute: Hashable {
-    case wardrobe                   // 27: 바로의 옷장
+    case profileEdit                // 34: 내 정보 수정 메뉴
     case nicknameEdit               // 30: 닉네임 수정
-    case policyRecommendationEdit   // 31: 정책 추천 기준 수정
-    case notificationSettings       // 25: 알림 설정
-    case appInfo                    // 26: 앱 정보
-    case termsOfService             // 26-1: 서비스 이용약관
-    case privacyPolicy              // 26-2: 개인정보처리방침
+    case socialMediaAccount         // 35: 연동된 소셜 계정
     case withdrawal                 // 32: 탈퇴
+    case baroCloset                 // 27: 바로의 옷장
+    case attendanceCalendar         // 28: 출석 달력
+    case policyRecommendationEdit   // 24: 정책 추천 기준 수정
+    case notificationSettings       // 25: 알림 설정
+    case termsOfService             // 26: 서비스 이용약관
+    case privacyPolicy              // 36: 개인정보처리방침
 }
--- Projects/BaziPresentation/Sources/Coordinator/Routes/SharedRoute.swift
+++ Projects/BaziPresentation/Sources/Coordinator/Routes/SharedRoute.swift
@@ -1,5 +1,6 @@
 // Copyright © 2026 ChungBazi. All rights reserved.
 
 public enum SharedRoute: Hashable {
+    case customPolicyList          // 12: 맞춤정책 더보기
     case policyDetail(id: String)  // 18: 정책 상세 (홈/검색/내정책 공유)
 }
