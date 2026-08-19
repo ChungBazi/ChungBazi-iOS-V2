85f89d882ceedab0cc8be07c1f8d558b71af472c
Hyeon-Ju <lhjivy1024@gmail.com>
2026-08-20 03:04:08 +0900

    [#30] feat: 분야별 맞춤 정책 조회 API 추가 및 홈 응답 최신화
    - getPersonalizedPolicies(category:) 추가 (GET /v1/home/policies/personalized)
    - PersonalizedPolicyResponseDTO 추가
    - HomePolicySectionResponseDTO에 hasUnreadNotification, personalizedPolicies 필드 반영

--- Projects/BaziNetwork/Sources/Home/PolicyListResponseDTO.swift
+++ Projects/BaziNetwork/Sources/Home/PolicyListResponseDTO.swift
@@ -3,10 +3,17 @@
 import Foundation
 
 public struct HomePolicySectionResponseDTO: Decodable {
+    public let hasUnreadNotification: Bool
+    public let personalizedPolicies: [PolicyItemDTO]
     public let recentViewedPolicies: [PolicyItemDTO]
     public let popularPolicies: [PolicyItemDTO]
     public let upcomingDeadlinePolicies: [PolicyItemDTO]
     public let latestPolicies: [PolicyItemDTO]
+}
+
+/// 분야별 맞춤 정책 목록 Response DTO
+public struct PersonalizedPolicyResponseDTO: Decodable {
+    public let policies: [PolicyItemDTO]
 }
 
 /// Home과 Search 결과 정책 리스트 Response를 담는 DTO
