78e33168e5c4ba64b44b0343a54d87d563c9f0c4
dlguszoo <lhjivy1024@gmail.com>
2026-08-20 22:44:39 +0900

    [#36] feat: 닉네임 수정(30) 화면 구현
    - 기존 nicknameClient 재사용, 저장 성공 시 화면 이동 없이 토스트만 표시
    - 기존 닉네임에서 실제로 바뀐 경우에만 저장하기 버튼 활성화

---
+++ Projects/BaziPresentation/Sources/Features/NicknameEdit/NicknameEditView.swift
@@ -0,0 +1,77 @@
+// Copyright © 2026 ChungBazi. All rights reserved.
+
+import SwiftUI
+
+import BaziDesign
+import ComposableArchitecture
+
+public struct NicknameEditView: View {
+
+    // MARK: - Properties
+
+    @Bindable var store: StoreOf<NicknameEditFeature>
+    @Environment(\.dismiss) private var dismiss
+
+    // MARK: - Init
+
+    public init(store: StoreOf<NicknameEditFeature>) {
+        self.store = store
+    }
+
+    // MARK: - Body
+
+    public var body: some View {
+        content
+            .baziNavigationBar_backWithTitle("닉네임 수정") {
+                dismiss()
+            }
+            .baziToast(isPresented: $store.isSuccessToastPresented, message: "닉네임이 수정되었어요")
+    }
+}
+
+// MARK: - Subviews
+
+extension NicknameEditView {
+
+    private var content: some View {
+        VStack(alignment: .leading, spacing: 20) {
+            titleText
+            inputField
+            Spacer()
+            saveButton
+        }
+        .padding(.horizontal, 20)
+        .padding(.top, 24)
+        .baziBackground(.bgWhite)
+    }
+
+    private var titleText: some View {
+        Text("어떤 이름으로 변경할까요?")
+            .baziFont(.head22B)
+            .foregroundStyle(Color.grayBlack)
+    }
+
+    private var inputField: some View {
+        BZInputField(text: $store.draftNickname, placeholder: "닉네임을 입력해주세요")
+    }
+
+    private var saveButton: some View {
+        BZButton("저장하기") {
+            store.send(.didTapSaveButton)
+        }
+        .disabled(!store.isSaveEnabled || store.isSaving)
+        .padding(.bottom, 5)
+    }
+}
+
+// MARK: - Preview
+
+#Preview {
+    NavigationStack {
+        NicknameEditView(
+            store: Store(initialState: .init(currentNickname: "민재")) {
+                NicknameEditFeature()
+            }
+        )
+    }
+}
