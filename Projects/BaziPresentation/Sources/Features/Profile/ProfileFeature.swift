e90487edabc2451b34f72ecd9d4d08f625a59ba5
dlguszoo <lhjivy1024@gmail.com>
2026-08-20 22:45:27 +0900

    [#36] feat: 프로필 메인(23) 화면 구현 및 하위 화면 연결
    - 닉네임/아바타 헤더, 알림 설정/정책 맞춤 조건 수정/이용약관/개인정보 처리방침 진입점
    - Path로 내 정보 수정, 닉네임 수정, 로그인된 소셜 계정, 탈퇴하기 등 모든 하위 화면 연결
    - 로그아웃/탈퇴 완료 delegate를 MainFeature까지 bubble해 로그인 화면으로 리셋되도록 배선

--- Projects/BaziPresentation/Sources/Features/Profile/ProfileFeature.swift
+++ Projects/BaziPresentation/Sources/Features/Profile/ProfileFeature.swift
@@ -5,17 +5,53 @@ import ComposableArchitecture
 @Reducer
 public struct ProfileFeature {
 
+    // MARK: - Navigation
+
+    @Reducer
+    public enum Path {
+        case infoEdit(ProfileInfoEditFeature)
+        case nicknameEdit(NicknameEditFeature)
+        case linkedAccounts(LinkedAccountsFeature)
+        case withdraw(WithdrawFeature)
+        case policyProfileEdit(PolicyProfileEditFeature)
+        case notificationSetting(NotificationSettingFeature)
+        case terms(TermsOfServiceFeature)
+        case privacy(PrivacyPolicyFeature)
+    }
+
     // MARK: - State
 
     @ObservableState
     public struct State: Equatable {
+        public var path = StackState<Path.State>()
+        // TODO: BaziDomain의 내 프로필 조회 UseCase가 준비되면 UserAPI.getProfile 응답으로 교체한다.
+        public var nickname = "김민재"
+
         public init() {}
     }
 
     // MARK: - Action
 
-    public enum Action: Equatable {
+    public enum Action {
+        // MARK: View
         case onAppear
+        case didTapProfileHeader
+        case didTapPolicyProfileRow
+        case didTapNotificationSettingRow
+        case didTapTermsRow
+        case didTapPrivacyRow
+
+        // MARK: Child
+        case path(StackActionOf<Path>)
+
+        // MARK: Delegate
+        case delegate(Delegate)
+    }
+
+    // MARK: - Delegate
+
+    public enum Delegate: Equatable {
+        case didLogout
     }
 
     // MARK: - Init
@@ -29,7 +65,56 @@ public struct ProfileFeature {
             switch action {
             case .onAppear:
                 return .none
+
+            case .didTapProfileHeader:
+                state.path.append(.infoEdit(ProfileInfoEditFeature.State()))
+                return .none
+
+            case .didTapPolicyProfileRow:
+                state.path.append(.policyProfileEdit(PolicyProfileEditFeature.State()))
+                return .none
+
+            case .didTapNotificationSettingRow:
+                state.path.append(.notificationSetting(NotificationSettingFeature.State()))
+                return .none
+
+            case .didTapTermsRow:
+                state.path.append(.terms(TermsOfServiceFeature.State()))
+                return .none
+
+            case .didTapPrivacyRow:
+                state.path.append(.privacy(PrivacyPolicyFeature.State()))
+                return .none
+
+            case .path(.element(_, .infoEdit(.delegate(.didTapNicknameEdit)))):
+                state.path.append(.nicknameEdit(NicknameEditFeature.State(currentNickname: state.nickname)))
+                return .none
+
+            case .path(.element(_, .infoEdit(.delegate(.didTapLinkedAccounts)))):
+                state.path.append(.linkedAccounts(LinkedAccountsFeature.State()))
+                return .none
+
+            case .path(.element(_, .infoEdit(.delegate(.didTapWithdraw)))):
+                state.path.append(.withdraw(WithdrawFeature.State()))
+                return .none
+
+            case .path(.element(_, .nicknameEdit(.delegate(.didSaveNickname(let name))))):
+                state.nickname = name
+                return .none
+
+            case .path(.element(_, .infoEdit(.delegate(.didLogout)))),
+                 .path(.element(_, .withdraw(.delegate(.didCompleteWithdrawal)))):
+                return .send(.delegate(.didLogout))
+
+            case .path:
+                return .none
+
+            case .delegate:
+                return .none
             }
         }
+        .forEach(\.path, action: \.path)
     }
 }
+
+extension ProfileFeature.Path.State: Equatable {}
