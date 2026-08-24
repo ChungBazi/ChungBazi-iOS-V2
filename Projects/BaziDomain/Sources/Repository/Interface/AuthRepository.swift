382efd94849c03e8e9f17a1528801b9f60206dc6
Hyeon-Ju <lhjivy1024@gmail.com>
2026-08-25 02:16:07 +0900

    [#33] chore: AuthRepository 주석 수정

--- Projects/BaziDomain/Sources/Repository/Interface/AuthRepository.swift
+++ Projects/BaziDomain/Sources/Repository/Interface/AuthRepository.swift
@@ -2,8 +2,7 @@
 
 import Foundation
 
-/// 서버와의 인증(로그인) 통신을 담당한다. 응답으로 받은 토큰은 이 계층에서 즉시 저장하고,
-/// 화면 분기에 필요한 계정 상태만 반환한다.
+/// 서버와의 인증(로그인) 통신을 담당한다. 응답으로 받은 토큰은 이 계층에서 즉시 저장하고, 화면 분기에 필요한 계정 상태만 반환한다.
 public protocol AuthRepository: Sendable {
     func kakaoLogin(accessToken: String, fcmToken: String) async throws -> AccountStatus
     func appleLogin(idToken: String, name: String, fcmToken: String) async throws -> AccountStatus
