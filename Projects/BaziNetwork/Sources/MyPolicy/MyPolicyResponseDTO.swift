4f834d3dfe054c7b11a35e20e229911859af0e23
Hyeon-Ju <lhjivy1024@gmail.com>
2026-08-30 00:51:53 +0900

    [#45] fix: MyPolicyResponseDTO에 Sendable 추가

--- Projects/BaziNetwork/Sources/MyPolicy/MyPolicyResponseDTO.swift
+++ Projects/BaziNetwork/Sources/MyPolicy/MyPolicyResponseDTO.swift
@@ -3,18 +3,18 @@
 import Foundation
 
 /// 마감이 다가오는 찜한 정책 Response DTO
-public struct MyPolicyDeadlineResponseDTO: Decodable {
+public struct MyPolicyDeadlineResponseDTO: Decodable, Sendable {
     public let policies: [PolicyItemDTO]
 }
 
 /// 캘린더 조회 Response DTO
-public struct CalendarResponseDTO: Decodable {
+public struct CalendarResponseDTO: Decodable, Sendable {
     public let targetMonth: String
     public let deadlineDates: [String]
 }
 
 /// 정책 메모 조회 Response DTO
-public struct PolicyMemoResponseDTO: Decodable {
+public struct PolicyMemoResponseDTO: Decodable, Sendable {
     public let policyId: Int
     public let category: String
     public let categoryName: String
