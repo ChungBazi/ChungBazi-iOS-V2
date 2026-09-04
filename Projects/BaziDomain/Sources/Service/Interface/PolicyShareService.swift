4c488af96442c29ae2a7d3e16442f7f737be97e4
Hyeon-Ju <lhjivy1024@gmail.com>
2026-09-05 01:28:59 +0900

    [#60] feat: 공유 썸네일 도메인 타입/인터페이스 추가
    - ShareThumbnail(cacheKey+imageData) 추가
    - PolicyShareService.makeKakaoShareURL(_:thumbnail:)로 확장

--- Projects/BaziDomain/Sources/Service/Interface/PolicyShareService.swift
+++ Projects/BaziDomain/Sources/Service/Interface/PolicyShareService.swift
@@ -4,5 +4,6 @@ import Foundation
 
 /// 카카오 공유 URL을 생성하는 서비스. 실제 앱 전환(open)은 상위 계층이 담당한다.
 public protocol PolicyShareService: Sendable {
-    func makeKakaoShareURL(_ content: PolicyShareContent) async throws -> URL
+    /// `thumbnail`이 있으면 카카오 서버에 업로드해 공유 이미지로 첨부한다(없으면 이미지 없이 공유).
+    func makeKakaoShareURL(_ content: PolicyShareContent, thumbnail: ShareThumbnail?) async throws -> URL
 }
